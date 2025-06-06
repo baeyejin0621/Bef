03c34b52a2e06378dfd593070011b6df7e7fb97e
baeyejin0621 <baeyejin5652@naver.com>
2025-06-07 00:47:09 +0900

    js로 빈 하트 누르면 채워진 하트로 바뀌면서 a 태그 동작 안 하는 기능 완성

--- js/click_display.js
+++ js/click_display.js
@@ -1,21 +1,39 @@
 "use strict";
 
-//A 요소 클릭하면 B 요소 나타나기
-//A
-const clickDisplay = document.querySelectorAll(".click_display");
-//B
-const display = document.querySelectorAll(".display");
+//헤더에서 돋보기 버튼 누르면 색깔 바뀌고 input 태그 나타나기
+const search = document.querySelector("#search");
 
-clickDisplay.forEach((element, i) => {
+search.addEventListener("click", () => {
+  if (
+    window.getComputedStyle(this.children[0]).display == "none" &&
+    window.getComputedStyle(this.children[2]).display == "block"
+  ) {
+    this.children[0].display = "block";
+    this.children[2].display = "block";
+  }
+});
+//안 먹힘
+
+//빈 하트 클릭하면 채워진 하트로 바뀌기
+const like = document.querySelectorAll(".like");
+
+like.forEach((element, i) => {
   element.addEventListener("click", (event) => {
-    if (element.style.display == "block") {
-      element.style.display = "none";
-      display[i].style.display = "block";
-      event.stopPropagation();
-    } else if (element.style.display == "none") {
-      element.style.display = "block";
-      display[i].style.display = "none";
-      event.stopPropagation();
+    if (
+      window.getComputedStyle(element.children[0].children[0]).display ==
+      "block"
+    ) {
+      element.children[0].children[0].style.display = "none";
+      element.children[0].children[1].style.display = "block";
+      element.children[1].innerText = Number(element.children[1].innerText) + 1;
+    } else if (
+      window.getComputedStyle(like[i].children[0].children[0]).display == "none"
+    ) {
+      element.children[0].children[0].style.display = "block";
+      element.children[0].children[1].style.display = "none";
+      element.children[1].innerText = Number(element.children[1].innerText) - 1;
     }
+
+    event.preventDefault();
   });
 });
