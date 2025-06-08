f2433a06e7ea41781eeaeb571e93df6b5b4c49a8
baeyejin0621 <baeyejin5652@naver.com>
2025-06-08 20:46:46 +0900

    돋보기 아이콘 누르면 검색어 입력창 나오는 기능 완성 하지만 원래 css로 만들어놨던 호버 기능이 사라짐

--- js/click_display.js
+++ js/click_display.js
@@ -3,31 +3,38 @@
 //헤더에서 돋보기 버튼 누르면 색깔 바뀌고 input 태그 나타나기
 const search = document.querySelector("#search");
 
-search.addEventListener("click", () => {
-  if (
-    window.getComputedStyle(this.children[0]).display == "none" &&
-    window.getComputedStyle(this.children[2]).display == "block"
-  ) {
-    this.children[0].display = "block";
-    this.children[2].display = "block";
+search.addEventListener("click", function () {
+  if (getComputedStyle(this.children[0]).display == "none") {
+    this.style.borderBottom = "2px solid var(--black-color6)";
+    this.children[0].style.display = "block";
+    this.children[1].style.display = "none";
+    this.children[2].style.display = "block";
+  } else if (getComputedStyle(this.children[0]).display == "block") {
+    this.style.borderBottom = "2px solid transparent";
+    this.children[0].style.display = "none";
+    this.children[1].style.display = "block";
+    this.children[2].style.display = "none";
   }
 });
-//안 먹힘
+
+//인풋 태그 눌렀을 때 버블링 막기
+search.children[0].addEventListener("click", (event) => {
+  if (getComputedStyle(search.children[0]).display == "block") {
+    event.stopPropagation();
+  }
+});
 
 //빈 하트 클릭하면 채워진 하트로 바뀌기
 const like = document.querySelectorAll(".like");
 
-like.forEach((element, i) => {
+like.forEach((element) => {
   element.addEventListener("click", (event) => {
-    if (
-      window.getComputedStyle(element.children[0].children[0]).display ==
-      "block"
-    ) {
+    if (getComputedStyle(element.children[0].children[0]).display == "block") {
       element.children[0].children[0].style.display = "none";
       element.children[0].children[1].style.display = "block";
       element.children[1].innerText = Number(element.children[1].innerText) + 1;
     } else if (
-      window.getComputedStyle(like[i].children[0].children[0]).display == "none"
+      getComputedStyle(element.children[0].children[0]).display == "none"
     ) {
       element.children[0].children[0].style.display = "block";
       element.children[0].children[1].style.display = "none";
