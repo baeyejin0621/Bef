1a98b0f105adc1c398cc2dcfe805ada39f13b48a
baeyejin0621 <baeyejin5652@naver.com>
2025-06-11 23:03:21 +0900

    Docs: gsap으로 숫자 카운트 효과 넣는 중

--- js/gsap/service_guide/service_guide1/gsap.js
+++ js/gsap/service_guide/service_guide1/gsap.js
@@ -4,123 +4,146 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger); //스크롤 트리거
 
-  //공격 & 방어 카드들
-  let tl = gsap.timeline({
+  //.sece3 pin 효과
+  let tl1 = gsap.timeline({
     scrollTrigger: {
       trigger: ".sec3",
-      start: "0% 15%",
-      end: "3000% 50%",
+      start: "62% 50%",
+      end: "1100% 50%",
       scrub: 1,
-      duration: 20,
       pin: true,
-      ease: "linear",
-      markers: true,
     },
   });
 
-  tl.fromTo(
-    ".attack1",
-    {
-      opacity: 0,
-      y: -200,
-    },
-    {
-      opacity: 1,
-      y: 0,
-    },
-    "+=.5"
-  );
+  //공격 & 방어 카드들
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".sec3 .cards",
+        start: "50% 60%",
+        end: "3000% 50%",
+        scrub: 1,
+        duration: 20,
+        ease: "linear",
+      },
+    })
+    .fromTo(
+      ".attack1",
+      {
+        opacity: 0,
+        y: -200,
+      },
+      {
+        opacity: 1,
+        y: 0,
+      },
+      "+=.5"
+    )
+    .fromTo(
+      ".defend1",
+      {
+        opacity: 0,
+        y: 200,
+      },
+      {
+        opacity: 1,
+        y: 0,
+      },
+      "+=2"
+    )
+    .fromTo(
+      ".attack2",
+      {
+        opacity: 0,
+        y: -200,
+      },
+      {
+        opacity: 1,
+        y: 0,
+      },
+      "+=2"
+    )
+    .fromTo(
+      ".defend2",
+      {
+        opacity: 0,
+        y: 200,
+      },
+      {
+        opacity: 1,
+        y: 0,
+      },
+      "+=2"
+    )
+    .fromTo(
+      ".attack3",
+      {
+        opacity: 0,
+        y: -200,
+      },
+      {
+        opacity: 1,
+        y: 0,
+      },
+      "+=2"
+    )
+    .fromTo(
+      ".defend3",
+      {
+        opacity: 0,
+        y: 200,
+      },
+      {
+        opacity: 1,
+        y: 0,
+      },
+      "+=2"
+    )
+    .fromTo(
+      ".attack4",
+      {
+        opacity: 0,
+        y: -200,
+      },
+      {
+        opacity: 1,
+        y: 0,
+      },
+      "+=2"
+    )
+    .fromTo(
+      ".defend4",
+      {
+        opacity: 0,
+        y: 200,
+      },
+      {
+        opacity: 1,
+        y: 0,
+      },
+      "+=2"
+    );
 
-  tl.fromTo(
-    ".defend1",
-    {
-      opacity: 0,
-      y: 200,
-    },
-    {
-      opacity: 1,
-      y: 0,
-    },
-    "+=2"
-  );
+  /*서비스 소개 페이지 - sec4 숫자 주르륵 올라가는 효과*/
+  const numberUp1 = document.querySelector("#number_up1");
+  const numberUp2 = document.querySelector("#number_up2");
 
-  tl.fromTo(
-    ".attack2",
-    {
-      opacity: 0,
-      y: -200,
-    },
-    {
-      opacity: 1,
-      y: 0,
-    },
-    "+=2"
-  );
+  let numStart = 0;
 
-  tl.fromTo(
-    ".defend2",
-    {
-      opacity: 0,
-      y: 200,
+  gsap.to(numStart, {
+    numStart: 1547,
+    duration: 3,
+    ease: "linear",
+    onUpdate: numChange,
+    scrollTrigger: {
+      trigger: "footer",
+      start: "0% 100%",
+      end: "0% 100%",
+      scrub: 1,
     },
-    {
-      opacity: 1,
-      y: 0,
-    },
-    "+=2"
-  );
+  });
 
-  tl.fromTo(
-    ".attack3",
-    {
-      opacity: 0,
-      y: -200,
-    },
-    {
-      opacity: 1,
-      y: 0,
-    },
-    "+=2"
-  );
-
-  tl.fromTo(
-    ".defend3",
-    {
-      opacity: 0,
-      y: 200,
-    },
-    {
-      opacity: 1,
-      y: 0,
-    },
-    "+=2"
-  );
-
-  tl.fromTo(
-    ".attack4",
-    {
-      opacity: 0,
-      y: -200,
-    },
-    {
-      opacity: 1,
-      y: 0,
-    },
-    "+=2"
-  );
-
-  tl.fromTo(
-    ".defend4",
-    {
-      opacity: 0,
-      y: 200,
-    },
-    {
-      opacity: 1,
-      y: 0,
-    },
-    "+=2"
-  );
+  function numChange() {
+    numberUp1.innerText = numStart.toFixed();
+  }
 });
-
-/*서비스 소개 페이지 - sec4 숫자 주르륵 올라가는 효과*/
